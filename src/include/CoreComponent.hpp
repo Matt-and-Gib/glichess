aed013c5898ef6c22e8ac246972f36c5ec983ca1
Michael Bethke <gibsonbethke@gmail.com>
2022-09-15 22:51:48 -0500

    Disabled copy and move for core components

--- src/include/CoreComponent.hpp
+++ src/include/CoreComponent.hpp
@@ -4,6 +4,13 @@
 namespace glichess {
 	class CoreComponent {
 	public:
+		CoreComponent() = default;
+		CoreComponent(const CoreComponent&) = delete;
+		CoreComponent(CoreComponent&&) = delete;
+		CoreComponent& operator=(const CoreComponent&) = delete;
+		CoreComponent& operator=(CoreComponent&&) = delete;
+		virtual ~CoreComponent() = default;
+
 		virtual void Initialize() = 0;
 		virtual void Update() = 0;
 	};
--- src/include/GameLogic.hpp
+++ src/include/GameLogic.hpp
@@ -8,6 +8,13 @@ namespace glichess {
 	private:
 
 	public:
+		GameLogic() = default;
+		GameLogic(const GameLogic&) = delete;
+		GameLogic(GameLogic&&) = delete;
+		GameLogic& operator=(const GameLogic&) = delete;
+		GameLogic& operator=(GameLogic&&) = delete;
+		~GameLogic() = default;
+
 		void Initialize() override;
 		void Update() override;
 	};
